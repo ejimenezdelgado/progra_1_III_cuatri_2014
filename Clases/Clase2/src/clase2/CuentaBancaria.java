b511039551f0e5389715f470d9ed60501debd3ef
ejimenezdelgado <ejimenezdelgado@hotmail.com>
2014-09-18 21:01:23 -0600

    Primera clase programada

---
+++ Clases/Clase2/src/clase2/CuentaBancaria.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clase2;
+
+/**
+ *
+ * @author Jimenez
+ */
+public class CuentaBancaria {
+    
+    private double saldoInicial;
+    private boolean hayError;
+    
+    public CuentaBancaria()
+    {
+        saldoInicial=0;
+        hayError=false;
+    }
+
+   
+}
