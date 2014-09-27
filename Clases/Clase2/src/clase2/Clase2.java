ba0ad6b321ad5a2e7b1126ce7883af207f5ef2bd
ejimenezdelgado <ejimenezdelgado@hotmail.com>
2014-09-26 18:26:02 -0600

    Clase 2

--- Clases/Clase2/src/clase2/Clase2.java
+++ Clases/Clase2/src/clase2/Clase2.java
@@ -6,6 +6,8 @@
 
 package clase2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jimenez
@@ -17,6 +19,52 @@ public class Clase2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        boolean validar=true;
+        int opcion;
+        char continuar;
+        double monto;
+        
+        CuentaBancaria oCuentaBancaria=new CuentaBancaria();
+        Scanner teclado=new Scanner(System.in);     
+        while(validar)
+        {
+            System.out.println("Digite 1 si quiere realizar un depósito y 2 si quiere realizar un retiro");
+            teclado=new Scanner(System.in);    
+            opcion=teclado.nextInt();
+            
+            if(opcion==1)
+            {
+                System.out.println("Digite el monto a depósitar");
+                teclado=new Scanner(System.in);    
+                monto=teclado.nextDouble();
+                oCuentaBancaria.Deposito(monto);
+            }
+            
+             if(opcion==2)
+            {
+                System.out.println("Digite el monto a retirar");
+                teclado=new Scanner(System.in);    
+                monto=teclado.nextDouble();
+                oCuentaBancaria.Retiro(monto);
+                if(oCuentaBancaria.isHayError())
+                {
+                   System.out.println("El monto que usted va a retirar no puede ser retirado"); 
+                }
+            }
+            
+             System.out.println("El saldo final es de "+oCuentaBancaria.getSaldoInicial()); 
+             System.out.println("Desea continuar con otra transacción S/N "); 
+             teclado=new Scanner(System.in);    
+             continuar=teclado.nextLine().charAt(0);
+             if((continuar=='S')||(continuar=='s'))
+             {
+                 validar=true;                
+             }     
+             if((continuar=='N')||(continuar=='n'))
+                 validar=false;
+        }
+        
     }
     
 }
--- Clases/Clase2/src/clase2/CuentaBancaria.java
+++ Clases/Clase2/src/clase2/CuentaBancaria.java
@@ -20,6 +20,42 @@ public class CuentaBancaria {
         saldoInicial=0;
         hayError=false;
     }
+    
+    /**
+     * Este método sirve para aumentar la cuenta de un usuario
+     * @param monto Parámetro para recibir el nuevo monto
+     */
+    public void Deposito(double monto)
+    {
+        setSaldoInicial(getSaldoInicial() + monto);
+    }
+    
+    /**
+     * Este método sirve para dismunuir la cuenta de un usuario
+     * @param monto Parámetro para recibir el nuevo monto
+     */
+    public void Retiro(double monto)
+    {
+        if(getSaldoInicial()>=monto)
+            setSaldoInicial(getSaldoInicial() - monto);
+        else
+            setHayError(true);
+    }
+    
+    public double getSaldoInicial() {
+        return saldoInicial;
+    }
 
+    public void setSaldoInicial(double saldoInicial) {
+        this.saldoInicial = saldoInicial;
+    }
+
+    public boolean isHayError() {
+        return hayError;
+    }
+
+    public void setHayError(boolean hayError) {
+        this.hayError = hayError;
+    }
    
 }
