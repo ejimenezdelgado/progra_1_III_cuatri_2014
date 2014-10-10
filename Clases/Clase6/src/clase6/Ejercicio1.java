b69d21a09b3c6a6423e18c18ad24d6c8355091c2
ejimenezdelgado <ejimenezdelgado@hotmail.com>
2014-10-09 21:12:17 -0600

    Clase 7 y 6

---
+++ Clases/Clase6/src/clase6/Ejercicio1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clase6;
+
+/**
+ *
+ * @author Jimenez
+ */
+ /* Realizar una clase, que permita cargar una dirección de mail en el constructor, 
+    luego en otro método mostrar un mensaje si contiene el caracter '@'.
+ */
+public class Ejercicio1 {
+    
+    private String email;
+        
+    public Ejercicio1(String pEmail)
+    {
+        this.email=pEmail;
+    }
+    
+    public String ValidarCorrreo()
+    {
+        String mensaje="El correo no contiene @";
+        for (int i = 0; i < email.length(); i++) {
+            if(email.charAt(i)=='@')
+            {
+                mensaje="El correo si contiene @";
+            }            
+        }
+        return mensaje;         
+    }
+    
+}
